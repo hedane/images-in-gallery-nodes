a92e48ac18265e251602664645fb4293f6c996b3
heduan <heduan@idaddy.cn>
2024-07-26 13:02:58 +0800

    [feature]add <image-metadata> node

--- __init__.py
+++ __init__.py
@@ -1,3 +1,5 @@
 from .images_in_gallery import ImagesInGalleryInvocation
 
 from .transpose_images import TransposeImagesInvocation
+
+from .image_metadata import ImageMetadataInvocation
---
+++ image_metadata.py
@@ -0,0 +1,24 @@
+# Copyright (c) 2024 hedane (https://github.com/hedane)
+
+from invokeai.invocation_api import (
+    BaseInvocation,
+    ImageField,
+    InputField,
+    InvocationContext,
+    invocation,
+    MetadataField,
+    MetadataOutput,
+)
+
+
+@invocation("image_metadata", title="Image Metadata", tags=["image", "metadata"], category="primitives", version="1.0.0")
+class ImageMetadataInvocation(BaseInvocation):
+    """get metadata of image"""
+
+    image: ImageField = InputField(description="The image to get metadata")
+
+    def invoke(self, context: InvocationContext) -> MetadataOutput:
+        metadata: MetadataField = context.images.get_metadata(self.image.image_name)
+        if metadata is None:
+            metadata = MetadataField.model_validate({})
+        return MetadataOutput(metadata=metadata)
